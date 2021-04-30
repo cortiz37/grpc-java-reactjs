f1f1eb6aabcd461b064e2601bd2b70da46102b72
cortiz37 <cortiz37@gmail.com>
2021-04-30 15:42:32 -0500

    operation per id.

--- server/src/main/java/com/sensor/repository/SensorRepository.java
+++ server/src/main/java/com/sensor/repository/SensorRepository.java
@@ -1,14 +1,15 @@
 package com.sensor.repository;
 
 import com.sensor.Sensor;
+import com.sensor.SensorData;
 import com.sensor.SensorType;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class SensorRepository {
 
     private final List<Sensor> sensors = new ArrayList<>();
+    private final Set<String> sensorIds = new HashSet<>();
 
     public SensorRepository() {
         Sensor.Builder builder = Sensor.newBuilder();
@@ -17,9 +18,23 @@ public class SensorRepository {
         sensors.add(builder.setId("003").setName("Room movement").setType(SensorType.PULSE).build());
         sensors.add(builder.setId("004").setName("Humidity").setType(SensorType.PULSE).build());
         sensors.add(builder.setId("005").setName("Wind speed").setType(SensorType.DIGITAL).build());
+
+        sensorIds.addAll(Arrays.asList("001", "002", "003", "004", "005"));
     }
 
     public List<Sensor> getSensors() {
         return sensors;
+    }
+
+    public SensorData getSensorData(String sensorId) {
+        Random random = new Random();
+        return SensorData.newBuilder()
+            .setValue(random.nextInt())
+            .setTimestamp(System.currentTimeMillis())
+            .build();
+    }
+
+    public boolean isSensorPresent(String sensorId) {
+        return sensorIds.contains(sensorId);
     }
 }
--- server/src/main/java/com/sensor/service/SensorServiceImpl.java
+++ server/src/main/java/com/sensor/service/SensorServiceImpl.java
@@ -1,7 +1,9 @@
 package com.sensor.service;
 
 import com.google.protobuf.Empty;
+import com.google.protobuf.StringValue;
 import com.sensor.Sensor;
+import com.sensor.SensorData;
 import com.sensor.SensorServiceGrpc;
 import com.sensor.repository.SensorRepository;
 import io.grpc.stub.StreamObserver;
@@ -17,6 +19,22 @@ public class SensorServiceImpl extends SensorServiceGrpc.SensorServiceImplBase {
     @Override
     public void getAll(Empty request, StreamObserver<Sensor> responseObserver) {
         sensorRepository.getSensors().forEach(responseObserver::onNext);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void streamById(StringValue request, StreamObserver<SensorData> responseObserver) {
+        if (sensorRepository.isSensorPresent(request.getValue())) {
+            while (true) {
+                SensorData sensorData = sensorRepository.getSensorData(request.getValue());
+                responseObserver.onNext(sensorData);
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
         responseObserver.onCompleted();
     }
 }
